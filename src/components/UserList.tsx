11fd3aac8faf1cfb79e795a89148b347c2530bcc
marco antonio <dinizsilvam3@gmail.com>
2024-12-10 04:19:53 -0300

    feature: 04-server-axios

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {IUser} from "../models/IUser"
 import { UserService } from '../services/UserService';
-import { useEffect } from 'react';
 
 interface IState {
     loading : boolean;
@@ -20,19 +19,60 @@ let UserList:React.FC<IProps> = () => {
     useEffect(() => {
         setState({...state, loading : true});
         UserService.getAllUsers().then((response) => {
-            setState({
-                ...state, 
+            setState(prevState =>({
+                ...prevState, 
                 loading : false,
-                users : response.data
-            })
+                users : response.data,
+                errorMessage : ''
+            }));
         }).catch(() => {
-            
+            setState(prevState =>({
+                ...prevState,
+                loading : false,
+                errorMessage : ''
+            }));
         });
-    }, );
+    }, []);
+
+    let {loading, users, errorMessage} = state;
 
     return (
         <React.Fragment>
             <h3>UserList</h3>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <table className='table table-hover text-center table-striped'>
+                            <thead>
+                                <tr>
+                                    <th>SNO</th>
+                                    <th>Name</th>
+                                    <th>Email</th>
+                                    <th>Phone</th>
+                                    <th>Company</th>
+                                    <th>Website</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    users.length > 0 && users.map(user => {
+                                        return (
+                                            <tr key={user.id}>
+                                                <td>{user.id}</td>
+                                                <td>{user.name}</td>
+                                                <td>{user.email}</td>
+                                                <td>{user.phone}</td>
+                                                <td>{user.company.name}</td>
+                                                <td>{user.website}</td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+                            </tbody>                            
+                        </table>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
     )
 }
